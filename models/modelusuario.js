84f2a4e79442b3da4e04b81a14860fc9c03cc421
Petizo Yiyo <petizo999@gmail.com>
2024-02-06 13:24:48 -0300

    backend soft jobs

---
+++ models/modelusuario.js
@@ -0,0 +1,36 @@
+import "dotenv/config";
+import format from "pg-format";
+import { pool } from "../database/connection.js";
+
+// Creamos una constante con la URL de la aplicación según el entorno
+// DOMAIN_URL_APP deberías crearla al momento de desplegar la aplicación
+// PORT se debe agregar al archivo .env
+const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? process.env.DOMAIN_URL_APP
+    : `http://localhost::${process.env.PORT}`
+
+const findOneEmail = async (email) => {
+  const query = "SELECT * FROM usuarios WHERE email = $1"
+  const { rows } = await pool.query(query, [email])
+  return rows[0]
+}
+
+const create = async ({ email, password, rol, lenguage }) => {
+
+  const query = "INSERT INTO usuarios (email, password, rol, lenguage) VALUES ($1, $2, $3, $4) RETURNING *"
+  const { rows } = await pool.query(query, [
+    email,
+    password,
+    rol,
+    lenguage,
+  ])
+  return rows[0]
+}
+
+
+
+export const modelUsuario = {
+  findOneEmail,
+  create,
+}
